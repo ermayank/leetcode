109c47160465f998b820e49ef020139aea6e2997
Mayank Gupta <mgupta.pkg@gmail.com>
2024-04-04 23:34:54 -0400

    Time: 59 ms (67.82%), Space: 51.7 MB (55.65%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -3,17 +3,27 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    let ht = {}
+    //Hash Map
+//     let ht = {}
     
-    for(let i of nums){
-        ht[i] = ht[i] + 1 || 1
-    }
+//     for(let i of nums){
+//         ht[i] = ht[i] + 1 || 1
+//     }
     
-    for(key in ht){
-        if (ht[key] == 1){ 
-            return key
-        }
+//     for(key in ht){
+//         if (ht[key] == 1){ 
+//             return key
+//         }
        
+//     }
+    
+    //XOR Operator
+    
+    let ans;
+    for (let i of nums) {
+        ans^=i;
+        
     }
+    return ans
 
 };

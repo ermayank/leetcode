1ba896917d1c0574e143a6aa2dc02cf645085c65
Mayank Gupta <30024008+ermayank@users.noreply.github.com>
2024-04-20 18:29:06 -0400

    Time: 68 ms (77.03%), Space: 52.3 MB (78.84%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.js
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.js
@@ -5,7 +5,7 @@
 var titleToNumber = function(columnTitle) {
     
     // This process is similar to binary-to-decimal conversion
-    //Getting Char Code and then subtracting 'A' to get eg B=2
+    //Getting Char Code and then subtracting 'A' to get eg B = 2
     let result = 0;
     for (let i = 0; i < columnTitle.length; i++)
     {

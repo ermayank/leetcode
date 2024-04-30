b7ad3763f21e290863a8ae8d4cce3295f0909195
Mayank Gupta <30024008+ermayank@users.noreply.github.com>
2024-04-30 17:50:43 -0400

    Time: 51 ms (84.74%), Space: 49.6 MB (36.56%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.js
+++ 0191-number-of-1-bits/0191-number-of-1-bits.js
@@ -5,8 +5,18 @@
 var hammingWeight = function(n) {
     let count = 0;
     let numBin = n.toString(2);
-    for(let c of numBin){
-     if (c === '1') count++   
-    }
+    
+    //Normal Loop
+    // for(let c of numBin){
+    //  if (c === '1') count++   
+    // }
+    // return count
+    
+    // Bitwise Shifting
+        while (n !== 0) {
+        const bitComparison = n & 1; // 1 & 1 will return 1. 0 & 1 will return 0.
+        if (bitComparison === 1) count++;
+        n >>>= 1; // unsigned right shift assignment (chop off the last bit and assign it)
+    }  
     return count
 };

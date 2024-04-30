3362a37cbe22e70d013435443a9601c534c6bd25
Mayank Gupta <30024008+ermayank@users.noreply.github.com>
2024-04-30 19:07:56 -0400

    Time: 86 ms (22.61%), Space: 55.1 MB (12.06%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let nonZeroPos = 0
+    
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] != 0){
+            [nums[nonZeroPos], nums[i]] = [nums[i], nums[nonZeroPos]]
+            nonZeroPos++
+        }    
+    }
+    
+};

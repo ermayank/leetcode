11c6e96ab4c9dac54d9af3aeb202d8f43dc59ff4
Mayank Gupta <mgupta.pkg@gmail.com>
2024-07-21 20:32:11 -0400

    Time: 107 ms (46.87%), Space: 65 MB (36.35%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    let res = new Array(nums.length).fill(1);
+    
+    let prefix = 1;
+    for (let i = 0; i < nums.length; i++){
+        res[i] = prefix;
+        prefix = prefix * nums[i]
+    }
+    
+    let postfix = 1;
+    for(let i = nums.length - 1; i >= 0; i--){
+        res[i] = res[i] * postfix;
+        postfix = postfix * nums[i]
+        
+    }
+    return res
+};

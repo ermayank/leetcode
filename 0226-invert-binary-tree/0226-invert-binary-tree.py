27f25d0df936f9dbd6cf170ad1ef43ec61f401c8
Mayank Gupta <mgupta.pkg@gmail.com>
2024-08-19 23:43:59 -0400

    Create 0226-invert-binary-tree.py

---
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        #Iterative
+        prev, current = None, head
+        
+        while current:
+            nxt = current.next
+            current.next = prev
+            prev = current
+            current = nxt
+        return prev

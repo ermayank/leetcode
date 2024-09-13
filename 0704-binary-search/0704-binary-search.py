91fb045d4f52b8d8cce63959adbc786dee7f9ccd
Mayank Gupta <mgupta.pkg@gmail.com>
2024-09-13 13:38:25 -0400

    Time: 186 ms (81.34%), Space: 18 MB (94.14%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.py
@@ -0,0 +1,16 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # Binary Search
+        l, r = 0, len(nums)-1
+        
+        while l <= r:
+            mid = l + ((r - l)//2)
+            
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                r = mid - 1
+            else:
+                l = mid + 1
+        return -1
+        

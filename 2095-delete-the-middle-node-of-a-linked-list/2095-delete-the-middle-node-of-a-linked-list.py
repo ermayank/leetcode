4bb348461f49e025204f2f56844aecac32d5a86b
Mayank Gupta <mgupta.pkg@gmail.com>
2024-09-15 12:18:41 -0400

    Time: 623 ms (37.05%), Space: 50.4 MB (65.31%) - LeetHub

---
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # Trick is to start fast pointer 2 places advance
+        if not head.next:
+            return None
+        
+        slow, fast = head, head.next.next
+        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            
+        slow.next = slow.next.next
+        
+        return head
+        
